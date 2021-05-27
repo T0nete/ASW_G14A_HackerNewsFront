cf4380ad4b328ba7d1ab9aaa2a4c8ca83bd3eb42
Ubuntu <ubuntu@ip-172-31-30-46.ec2.internal>
2021-05-27 20:36:30 +0000

    deploy

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const express = require('express');
-const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
